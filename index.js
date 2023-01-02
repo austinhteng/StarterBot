2cb23ed7b6de744e55c1483a39f9c6783ccee473
Replit user <>
2023-01-02 23:59:47 +0000

    Code changes from 2023-01-02_23:59:47

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ function configureBot(bot) {
         bot.chat('start search')
         let countAfter = countBefore;
         while (countAfter <= countBefore) {
-            const foundEntity = await bot.findBlocks({blockNames: entityName, maxDistance: 80 });
+            const foundEntity = await bot.findBlocks({blockNames: entityName, maxDistance: 80 }).shift();
             if (foundEntity) {
                 // If the Bot located one, then go chop it
-                const success = await bot.findAndDigBlock(foundEntity, { maxDistance: 80 }).shift();
+                const success = await bot.findAndDigBlock(foundEntity, { maxDistance: 80 });
                 if (!success) {
                     // If anything prevents the Bot from breaking the block,
                     // then find the next-closest and try gathering that instead.
